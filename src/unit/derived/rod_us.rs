64680a8d3bc297a54c0ffd8b5eeb82929508a4e3
Steve Loveless <steve@agrian.com>
2017-08-04 11:27:30 -0700

    Add missing ft_us, rd_us units

--- src/unit/derived/rod_us.rs
+++ src/unit/derived/rod_us.rs
@@ -4,19 +4,19 @@ use property::Property;
 use unit::{Definition, Unit, UnitType};
 
 #[derive(Debug, Default)]
-pub struct AcreUS;
+pub struct RodUS;
 
-impl Unit for AcreUS {
+impl Unit for RodUS {
     fn classification(&self) -> Classification { Classification::USLengths }
-    fn definition(&self) -> Definition { Definition::new(160.0, "[rd_us]2") }
+    fn definition(&self) -> Definition { Definition::new(16.6, "[ft_us]") }
     fn dim(&self) -> Dimension { Dimension::Length }
     fn is_arbitrary(&self) -> bool { false }
     fn is_metric(&self) -> bool { false }
     fn is_special(&self) -> bool { false }
-    fn names(&self) -> Vec<String> { vec!["acre".to_string()] }
-    fn primary_code(&self) -> String { "[acr_us]".to_string() }
+    fn names(&self) -> Vec<String> { vec!["rod".to_string()] }
+    fn primary_code(&self) -> String { "[rd_us]".to_string() }
     fn print_symbol(&self) -> Option<String> { None }
-    fn property(&self) -> Property { Property::Area }
-    fn secondary_code(&self) -> String { "[ACR_US]".to_string() }
+    fn property(&self) -> Property { Property::Length }
+    fn secondary_code(&self) -> String { "[RD_US]".to_string() }
     fn unit_type(&self) -> UnitType { UnitType::Derived }
 }
