4758425e86a1d4770da4f9d03af86f81499ed5b1
Steve Loveless <steve@agrian.com>
2017-08-13 08:05:20 -0700

    initial parser

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,15 @@
 #[macro_use]
 extern crate pretty_assertions;
 
+extern crate lalrpop_util;
+
 pub mod classification;
 pub mod dimension;
 pub mod measurement;
 pub mod parser_terms;
 pub mod parser;
 pub mod property;
+pub mod summary_unit;
 pub mod unit;
 
 pub use measurement::Measurement;
