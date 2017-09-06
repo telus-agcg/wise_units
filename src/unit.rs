868091f29702f42cc3703a16a1df06bf95774e12
Steve Loveless <steve@agrian.com>
2017-09-06 13:09:19 -0700

    Add tests and fix unit

--- src/unit.rs
+++ src/unit.rs
@@ -12,7 +12,7 @@ impl Unit {
     pub fn is_special(&self) -> bool {
         self.terms.iter().any(|ref term| {
             match term.atom {
-                Some(ref unit_container) => unit_container.is_special(),
+                Some(ref atom) => atom.is_special(),
                 None => false
             }
         })
@@ -26,19 +26,19 @@ impl Unit {
         self.calculate_magnitude(self.scalar())
     }
 
-    pub fn calculate_scalar(&self, magnitude: f64) -> f64 {
+    pub fn calculate_scalar(&self, value: f64) -> f64 {
         self.terms.iter()
             .fold(1.0, |acc, ref term| {
                 let term = *term;
-                acc * term.calculate_scalar(magnitude)
+                acc * term.calculate_scalar(value)
             })
     }
 
-    pub fn calculate_magnitude(&self, scalar: f64) -> f64 {
+    pub fn calculate_magnitude(&self, value: f64) -> f64 {
         self.terms.iter()
             .fold(1.0, |acc, ref term| {
                 let term = *term;
-                acc * term.calculate_magnitude(scalar)
+                acc * term.calculate_magnitude(value)
             })
     }
 
@@ -75,30 +75,192 @@ impl fmt::Display for Unit {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use composition::Composition;
     use interpreter::Interpreter;
+    use dimension::Dimension;
 
     #[test]
     fn validate_is_special() {
         let mut i = Interpreter;
-        let su = i.interpret("m");
+        let unit = i.interpret("m");
 
-        assert!(!su.is_special());
+        assert!(!unit.is_special());
     }
 
     #[test]
     fn validate_scalar() {
         let mut i = Interpreter;
-        let su = i.interpret("m");
 
-        assert_eq!(su.scalar(), 1.0);
+        let unit = i.interpret("m");
+        assert_eq!(unit.scalar(), 1.0);
+
+        let unit = i.interpret("km");
+        assert_eq!(unit.scalar(), 1000.0);
+
+        let unit = i.interpret("km/10m");
+        assert_eq!(unit.scalar(), 100.0);
+
+        let unit = i.interpret("m-1");
+        assert_eq!(unit.scalar(), 1.0);
+
+        let unit = i.interpret("10m");
+        assert_eq!(unit.scalar(), 10.0);
+
+        let unit = i.interpret("10km");
+        assert_eq!(unit.scalar(), 10_000.0);
+
+        let unit = i.interpret("10km-1");
+        assert_eq!(unit.scalar(), 0.0001);
+
+        let unit = i.interpret("km-1/m2");
+        assert_eq!(unit.scalar(), 0.001);
+
+        let unit = i.interpret("km/m2.cm");
+        assert_eq!(unit.scalar(), 100_000.0);
+
+        let unit = i.interpret("km-1/m2.cm");
+        assert_eq!(unit.scalar(), 0.1);
+
+        let unit = i.interpret("m/s2");
+        assert_eq!(unit.scalar(), 1.0);
+
+        // TODO: Interpreter needs to parse using Rule::main_term, not Rule::term
+        // let unit = i.interpret("/{tot}");
+        // assert_eq!(unit.scalar(), 1.0);
     }
 
     #[test]
     fn validate_magnitude() {
         let mut i = Interpreter;
-        let su = i.interpret("m");
 
-        assert_eq!(su.magnitude(), 1.0);
+        let unit = i.interpret("m");
+        assert_eq!(unit.magnitude(), 1.0);
+
+        let unit = i.interpret("km");
+        assert_eq!(unit.magnitude(), 1000.0);
+
+        let unit = i.interpret("km/10m");
+        assert_eq!(unit.magnitude(), 100.0);
+
+        let unit = i.interpret("m-1");
+        assert_eq!(unit.magnitude(), 1.0);
+
+        let unit = i.interpret("10m");
+        assert_eq!(unit.magnitude(), 10.0);
+
+        let unit = i.interpret("10km");
+        assert_eq!(unit.magnitude(), 10_000.0);
+
+        let unit = i.interpret("10km-1");
+        assert_eq!(unit.magnitude(), 0.0001);
+
+        let unit = i.interpret("km-1/m2");
+        assert_eq!(unit.magnitude(), 0.001);
+
+        let unit = i.interpret("km/m2.cm");
+        assert_eq!(unit.magnitude(), 100_000.0);
+
+        let unit = i.interpret("km-1/m2.cm");
+        assert_eq!(unit.magnitude(), 0.1);
+
+        let unit = i.interpret("m/s2");
+        assert_eq!(unit.magnitude(), 1.0);
+
+        // TODO: Interpreter needs to parse using Rule::main_term, not Rule::term
+        // let unit = i.interpret("/{tot}");
+        // assert_eq!(unit.magnitude(), 1.0);
+    }
+
+    #[test]
+    fn validate_composition() {
+        let mut i = Interpreter;
+
+        let unit = i.interpret("m");
+        let composition = Composition::new_from_values(Dimension::Length, 1);
+        assert_eq!(unit.composition(), composition);
+
+        let unit = i.interpret("km");
+        let composition = Composition::new_from_values(Dimension::Length, 1);
+        assert_eq!(unit.composition(), composition);
+
+        let unit = i.interpret("km/10m");
+        let composition = Composition::new_from_values(Dimension::Length, 0);
+        assert_eq!(unit.composition(), composition);
+
+        let unit = i.interpret("m-1");
+        let composition = Composition::new_from_values(Dimension::Length, -1);
+        assert_eq!(unit.composition(), composition);
+
+        let unit = i.interpret("10m");
+        let composition = Composition::new_from_values(Dimension::Length, 1);
+        assert_eq!(unit.composition(), composition);
+
+        let unit = i.interpret("10km");
+        let composition = Composition::new_from_values(Dimension::Length, 1);
+        assert_eq!(unit.composition(), composition);
+
+        let unit = i.interpret("10km-1");
+        let composition = Composition::new_from_values(Dimension::Length, -1);
+        assert_eq!(unit.composition(), composition);
+
+        let unit = i.interpret("km-1/m2");
+        let composition = Composition::new_from_values(Dimension::Length, -3);
+        assert_eq!(unit.composition(), composition);
+
+        let unit = i.interpret("km/m2.cm");
+        let composition = Composition::new_from_values(Dimension::Length, -2);
+        assert_eq!(unit.composition(), composition);
+
+        let unit = i.interpret("km-1/m2.cm");
+        let composition = Composition::new_from_values(Dimension::Length, -4);
+        assert_eq!(unit.composition(), composition);
+
+        let unit = i.interpret("m/s2");
+        let mut composition = Composition::new_from_values(Dimension::Length, 1);
+        composition.insert(Dimension::Time, -2);
+        assert_eq!(unit.composition(), composition);
+
+        // TODO: Interpreter needs to parse using Rule::main_term, not Rule::term
+        // let unit = i.interpret("/{tot}");
+        // assert_eq!(unit.magnitude(), 1.0);
+    }
+
+    #[test]
+    fn validate_is_compatible_with() {
+        let mut i = Interpreter;
+
+        let meter = i.interpret("m");
+        let km = i.interpret("km");
+        assert!(meter.is_compatible_with(&km));
+
+        let km_per_10m = i.interpret("km/10m");
+        assert!(!meter.is_compatible_with(&km_per_10m));
+
+        let per_meter = i.interpret("m-1");
+        assert!(!meter.is_compatible_with(&per_meter));
+
+        let ten_meter = i.interpret("10m");
+        assert!(meter.is_compatible_with(&ten_meter));
+
+        let ten_km = i.interpret("10km");
+        assert!(meter.is_compatible_with(&ten_km));
+
+        let per_ten_km = i.interpret("10km-1");
+        assert!(!meter.is_compatible_with(&per_ten_km));
+
+        let per_meter_cubed = i.interpret("km-1/m2");
+        assert!(!meter.is_compatible_with(&per_meter_cubed));
+
+        let km_per_length_cubed = i.interpret("km/m2.cm");
+        assert!(!meter.is_compatible_with(&km_per_length_cubed));
+
+        let km_per_length_fourthed = i.interpret("km-1/m2.cm");
+        assert!(!meter.is_compatible_with(&km_per_length_fourthed));
+
+        let meter_per_second_squared = i.interpret("m/s2");
+        assert!(!meter.is_compatible_with(&meter_per_second_squared));
+
+        let km_cubed_per_nanometer_squared = i.interpret("km3/nm2");
+        assert!(meter.is_compatible_with(&km_cubed_per_nanometer_squared));
     }
 }
