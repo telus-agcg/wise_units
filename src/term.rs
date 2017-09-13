580c9ac7fa87d38e6f47ba39aa0f8b779b33bc4c
Steve Loveless <steve@agrian.com>
2017-09-12 17:58:05 -0700

    Add Display for Term

--- src/term.rs
+++ src/term.rs
@@ -1,6 +1,7 @@
 use atom::Atom;
 use composition::Composition;
 use prefix::Prefix;
+use std::fmt;
 
 #[derive(Debug, PartialEq)]
 pub struct Term {
@@ -92,18 +93,29 @@ impl Term {
     }
 }
 
-                let mut new_composition = Composition::new();
+impl fmt::Display for Term {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", extract_term_string(&self))
+    }
+}
 
-                for (dim, exp) in atom_composition.into_iter() {
-                    let atom_exp = if exp == 1 { 0 } else { exp };
-                    new_composition.insert(dim, atom_exp + self.exponent);
-                }
+fn extract_term_string(term: &Term) -> String {
+    let mut term_string = String::new();
+    if term.factor != 1 { term_string.push_str(&term.factor.to_string()) };
 
-                Some(new_composition)
-            },
-            None => None
+    if let Some(prefix) = term.prefix {
+        term_string.push_str(&prefix.to_string());
+    }
+
+    if let Some(atom) = term.atom {
+        if term.exponent == 1 {
+            term_string.push_str(&atom.to_string());
+        } else {
+            term_string.push_str(&format!("{}{}", atom, term.exponent));
         }
     }
+
+    term_string
 }
 
 #[cfg(test)]
@@ -204,5 +216,38 @@ mod tests {
         term.exponent = -1;
         let composition = Composition::new(Dimension::Length, -1);
         assert_eq!(term.composition().unwrap(), composition);
+    }
+
+    #[test]
+    fn validate_display() {
+        let term = Term::new(None, None);
+        assert_eq!(term.to_string().as_str(), "");
+
+        let term = Term::new(Some(Atom::Meter), None);
+        assert_eq!(term.to_string().as_str(), "m");
+
+        let term = Term::new(Some(Atom::Meter), Some(Prefix::Kilo));
+        assert_eq!(term.to_string().as_str(), "km");
+
+        let mut term = Term::new(Some(Atom::Meter), None);
+        term.exponent = -1;
+        assert_eq!(term.to_string().as_str(), "m-1");
+
+        let mut term = Term::new(Some(Atom::Meter), None);
+        term.exponent = -2;
+        assert_eq!(term.to_string().as_str(), "m-2");
+
+        let mut term = Term::new(Some(Atom::Meter), None);
+        term.factor = 10;
+        assert_eq!(term.to_string().as_str(), "10m");
+
+        let mut term = Term::new(Some(Atom::Meter), Some(Prefix::Kilo));
+        term.factor = 10;
+        assert_eq!(term.to_string().as_str(), "10km");
+
+        let mut term = Term::new(Some(Atom::Meter), Some(Prefix::Kilo));
+        term.factor = 10;
+        term.exponent = -1;
+        assert_eq!(term.to_string().as_str(), "10km-1");
     }
 }
