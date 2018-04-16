c2c7586b83e4ee999a1a1ad917534f31cc001e86
Steve Loveless <steve@agrian.com>
2018-04-16 16:22:00 -0700

    Got enough of build_atoms! implemented

---
+++ src/test_atom.rs
@@ -0,0 +1,13 @@
+// trace_macros!(true);
+build_atoms!(
+    name => Thing,
+    classification => Chemical,
+    definition => 2.0, "m2",
+    primary_code => "[thing]"
+    ;
+    name => Meow,
+    classification => Chemical,
+    definition => 2.0, "m2",
+    primary_code => "[meow]"
+);
+// trace_macros!(false);
