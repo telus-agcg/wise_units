89b23b9cd6d779f0c0ecb47ac2122451842e2cc7
Steve Loveless <steve@agrian.com>
2018-04-25 15:25:50 -0700

    Add some more symbols
    In the middle of it though, wondering why the parse isn't consuming my whole string, causing tests to fail.

--- unit_parser/src/symbol_interpreter.rs
+++ unit_parser/src/symbol_interpreter.rs
@@ -38,7 +38,7 @@ impl SymbolInterpreter {
                     symbol = self.visit_symbol(pair)?;
                 }
                 _ => {
-                    println!("visit_with_pairs: unreachable rule: {:?}", pair);
+                    println!("visit_with_pairs: unreachable rule: {:#?}", pair);
                     unreachable!()
                 }
             };
@@ -49,6 +49,7 @@ impl SymbolInterpreter {
 
     fn visit_symbol(&self, pair: Pair<Rule>) -> Result<Symbol, Error> {
         println!("SYMBOL");
+        println!("SYMBOL string: {}", pair.as_str());
         let mut symbol = Symbol::new();
 
         for inner_pair in pair.into_inner() {
@@ -64,7 +65,7 @@ impl SymbolInterpreter {
                     symbol.atom = Some(atom);
                 }
                 _ => {
-                    println!("visit_symbol: unreachable rule: {:?}", inner_pair);
+                    println!("visit_symbol: unreachable rule: {:#?}", inner_pair);
                     unreachable!()
                 },
             }
@@ -74,7 +75,8 @@ impl SymbolInterpreter {
     }
 
     fn visit_atom(&self, pair: Pair<Rule>) -> Result<Atom, Error> {
-        println!("VISIT ATOM");
+        println!("VISIT ATOM: {:#?}", &pair);
+        println!("VISIT ATOM string: {}", pair.as_str());
         let mut atom: Option<Atom> = None;
 
         for inner_pair in pair.into_inner() {
@@ -91,7 +93,7 @@ impl SymbolInterpreter {
                 // Derived units last.
                 Rule::pri_acre_br | Rule::sec_acre_br => Atom::AcreBR,
                 Rule::pri_acre_us | Rule::sec_acre_us => Atom::AcreUS,
-                Rule::ampere => Atom::AcreUS,
+                Rule::ampere => Atom::Ampere,
                 Rule::pri_are | Rule::sec_are => Atom::Are,
                 Rule::pri_astronomic_unit | Rule::sec_astronomic_unit => Atom::AstronomicUnit,
                 Rule::pri_unified_atom_mass_unit | Rule::sec_unified_atom_mass_unit => Atom::AtomicMassUnit,
@@ -105,26 +107,36 @@ impl SymbolInterpreter {
                 Rule::pri_bushel_br | Rule::sec_bushel_br => Atom::BushelBR,
                 Rule::pri_bushel_us | Rule::sec_bushel_us => Atom::BushelUS,
 
+                Rule::pri_calorie_at_15c | Rule::sec_calorie_at_15c => Atom::CalorieAt15C,
+                Rule::pri_calorie_at_20c | Rule::sec_calorie_at_20c => Atom::CalorieAt20C,
+                Rule::pri_calorie_it     | Rule::sec_calorie_it     => Atom::CalorieIT,
+                Rule::pri_calorie_th     | Rule::sec_calorie_th     => Atom::CalorieTH,
+                Rule::pri_calorie_m      | Rule::sec_calorie_m      => Atom::CalorieM,
+                Rule::pri_calorie        | Rule::sec_calorie        => Atom::Calorie,
+                Rule::pri_cicero         | Rule::sec_cicero         => Atom::Cicero,
                 Rule::pri_circular_mil_i | Rule::sec_circular_mil_i => Atom::CircularMilInternational,
-                Rule::pri_cord_i | Rule::sec_cord_i => Atom::CordInternational,
-                Rule::pri_cord_us | Rule::sec_cord_us => Atom::CordUS,
-                Rule::pri_cubic_foot_i | Rule::sec_cubic_foot_i => Atom::CubicFootInternational,
-                Rule::pri_cubic_inch_i | Rule::sec_cubic_inch_i => Atom::CubicInchInternational,
-                Rule::pri_cubic_yard_i | Rule::sec_cubic_yard_i => Atom::CubicYardInternational,
-                Rule::pri_cup_us | Rule::sec_cup_us => Atom::CupUS,
-                Rule::pri_curie | Rule::sec_curie => Atom::Curie,
+                Rule::pri_cord_i         | Rule::sec_cord_i         => Atom::CordInternational,
+                Rule::pri_cord_us        | Rule::sec_cord_us        => Atom::CordUS,
+                Rule::pri_cubic_foot_i   | Rule::sec_cubic_foot_i   => Atom::CubicFootInternational,
+                Rule::pri_cubic_inch_i   | Rule::sec_cubic_inch_i   => Atom::CubicInchInternational,
+                Rule::pri_cubic_yard_i   | Rule::sec_cubic_yard_i   => Atom::CubicYardInternational,
+                Rule::pri_cup_us         | Rule::sec_cup_us         => Atom::CupUS,
+                Rule::pri_curie          | Rule::sec_curie          => Atom::Curie,
 
-                Rule::pri_day | Rule::sec_day => Atom::Day,
-                Rule::pri_degree_celsius | Rule::sec_degree_celsius => Atom::DegreeCelsius,
-                // "[degF]"    | "[DEGF]"   => Atom::DegreeFahrenheit,
-                Rule::minute_angle => Atom::DegreeMinute,
-                Rule::second_angle => Atom::DegreeSecond,
-                // "[degRe]"   | "°Ré"      => Atom::DegreeReaumur,
-                Rule::pri_degree | Rule::sec_degree => Atom::Degree,
-                Rule::pri_dram_av | Rule::sec_dram_av => Atom::DramAV,
-                Rule::pri_dry_pint_us | Rule::sec_dry_pint_us => Atom::DryPintUS,
-                Rule::pri_dry_quart_us | Rule::sec_dry_quart_us => Atom::DryQuartUS,
-                Rule::pri_dyne | Rule::sec_dyne => Atom::Dyne,
+                Rule::pri_day               | Rule::sec_day               => Atom::Day,
+                Rule::pri_degree_celsius    | Rule::sec_degree_celsius    => Atom::DegreeCelsius,
+                Rule::pri_degree_fahrenheit | Rule::sec_degree_fahrenheit => Atom::DegreeFahrenheit,
+                Rule::degree_rankine                                      => Atom::DegreeRankine,
+                Rule::degree_reaumur                                      => Atom::DegreeReaumur,
+                Rule::minute_angle                                        => Atom::DegreeMinute,
+                Rule::second_angle                                        => Atom::DegreeSecond,
+                Rule::pri_degree       | Rule::sec_degree                 => Atom::Degree,
+                Rule::pri_didot        | Rule::sec_didot                  => Atom::Didot,
+                Rule::pri_dram_ap      | Rule::sec_dram_ap                => Atom::DramAP,
+                Rule::pri_dram_av      | Rule::sec_dram_av                => Atom::DramAV,
+                Rule::pri_dry_pint_us  | Rule::sec_dry_pint_us            => Atom::DryPintUS,
+                Rule::pri_dry_quart_us | Rule::sec_dry_quart_us           => Atom::DryQuartUS,
+                Rule::pri_dyne         | Rule::sec_dyne                   => Atom::Dyne,
 
                 Rule::pri_electronvolt | Rule::sec_electronvolt => Atom::Electronvolt,
                 Rule::pri_electron_mass | Rule::sec_electron_mass => Atom::ElectronMass,
@@ -172,6 +184,7 @@ impl SymbolInterpreter {
                 Rule::pri_inch_us | Rule::sec_inch_us => Atom::InchUS,
 
                 Rule::joule => Atom::Joule,
+
                 Rule::pri_kayser | Rule::sec_kayser => Atom::Kayser,
                 Rule::pri_knot_br | Rule::sec_knot_br => Atom::KnotBR,
                 Rule::pri_knot_i | Rule::sec_knot_i => Atom::KnotInternational,
@@ -180,6 +193,8 @@ impl SymbolInterpreter {
                 Rule::pri_long_hundredweight_av | Rule::sec_long_hundredweight_av => Atom::LongHundredweightAV,
                 Rule::pri_long_ton_av | Rule::sec_long_ton_av => Atom::LongTonAV,
                 Rule::pri_light_year | Rule::sec_light_year => Atom::LightYear,
+                Rule::pri_ligne | Rule::sec_ligne => Atom::Ligne,
+                Rule::pri_line | Rule::sec_line => Atom::Line,
                 Rule::pri_link_for_gunters_chain_br | Rule::sec_link_for_gunters_chain_br => Atom::LinkForGuntersChainBR,
                 Rule::pri_link_for_gunters_chain_us | Rule::sec_link_for_gunters_chain_us => Atom::LinkForGuntersChainUS,
                 Rule::pri_link_for_ramdens_chain_us | Rule::sec_link_for_ramdens_chain_us => Atom::LinkForRamdensChainUS,
@@ -214,8 +229,10 @@ impl SymbolInterpreter {
                 Rule::pri_newtonian_constant_of_gravitation | Rule::sec_newtonian_constant_of_gravitation => Atom::NewtonianConstantOfGravitation,
                 Rule::pri_ohm | Rule::sec_ohm => Atom::Ohm,
                 Rule::pri_oersted | Rule::sec_oersted => Atom::Oersted,
+                Rule::pri_ounce_ap | Rule::sec_ounce_ap => Atom::OunceAP,
                 Rule::pri_ounce_av | Rule::sec_ounce_av => Atom::OunceAV,
-                // "[oz_tr]"   | "[OZ_TR]"  => Atom::OunceTR,
+                Rule::pri_ounce_m | Rule::sec_ounce_m => Atom::OunceM,
+                Rule::pri_ounce_tr | Rule::sec_ounce_tr => Atom::OunceTR,
 
                 Rule::pri_pace_br | Rule::sec_pace_br => Atom::PaceBR,
                 Rule::pri_parsec | Rule::sec_parsec => Atom::Parsec,
@@ -229,19 +246,26 @@ impl SymbolInterpreter {
                 Rule::percent => Atom::Percent,
                 Rule::pri_permeability_of_vacuum | Rule::sec_permeability_of_vacuum => Atom::PermeabilityOfVacuum,
                 Rule::pri_permittivity_of_vacuum | Rule::sec_permittivity_of_vacuum => Atom::PermittivityOfVacuum,
-                // "[pwt_tr]"  | "[PWT_TR]" => Atom::PennyweightTR,
+                Rule::pri_pennyweight_tr | Rule::sec_pennyweight_tr => Atom::PennyweightTR,
                 // "[pH]"      | "[PH]"     => Atom::PH,
                 Rule::pri_phot | Rule::sec_phot => Atom::Phot,
+                Rule::pri_pica | Rule::sec_pica => Atom::Pica,
+                Rule::pri_pied | Rule::sec_pied => Atom::Pied,
                 Rule::pri_pint_br | Rule::sec_pint_br => Atom::PintBR,
                 Rule::pri_pint_us | Rule::sec_pint_us => Atom::PintUS,
                 Rule::pri_planck_constant | Rule::sec_planck_constant => Atom::PlanckConstant,
+                Rule::pri_point | Rule::sec_point => Atom::Point,
+                Rule::pri_pouce | Rule::sec_pouce => Atom::Pouce,
+                Rule::pri_pound_ap | Rule::sec_pound_ap => Atom::PoundAP,
                 Rule::pri_pound_av | Rule::sec_pound_av => Atom::PoundAV,
-                // "[lb_tr]"   | "[LB_TR]"  => Atom::PoundTR,
+                Rule::pri_pound_tr | Rule::sec_pound_tr => Atom::PoundTR,
                 Rule::pri_pound_force | Rule::sec_pound_force => Atom::PoundForce,
                 Rule::poise => Atom::Poise,
                 // "[p'diop]"  | "[P'DIOP]" => Atom::PrismDiopter,
                 // "[PNU]"                  => Atom::ProteinNitrogenUnit,
                 Rule::pri_proton_mass | Rule::sec_proton_mass => Atom::ProtonMass,
+                Rule::pri_printers_pica | Rule::sec_printers_pica => Atom::PrintersPica,
+                Rule::pri_printers_point | Rule::sec_printers_point => Atom::PrintersPoint,
 
                 Rule::pri_quart_br | Rule::sec_quart_br => Atom::QuartBR,
                 Rule::pri_quart_us | Rule::sec_quart_us => Atom::QuartUS,
@@ -253,6 +277,7 @@ impl SymbolInterpreter {
                 Rule::pri_rod_us | Rule::sec_rod_us => Atom::RodUS,
                 Rule::pri_roentgen | Rule::sec_roentgen => Atom::Roentgen,
 
+                Rule::pri_scruple | Rule::sec_scruple => Atom::ScrupleAP,
                 Rule::pri_section | Rule::sec_section => Atom::Section,
                 Rule::pri_short_hundredweight_av | Rule::sec_short_hundredweight_av => Atom::ShortHundredweightAV,
                 Rule::pri_short_ton_av | Rule::sec_short_ton_av => Atom::ShortTonAV,
