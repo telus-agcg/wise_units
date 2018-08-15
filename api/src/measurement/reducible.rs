019a667a4c0b4475b61061fe58ee0600664d770a
Steve Loveless <steve@agrian.com>
2018-08-15 12:06:03 -0700

    DEV-331 Add tests for impl Reducible for Measurement

--- api/src/measurement/reducible.rs
+++ api/src/measurement/reducible.rs
@@ -20,3 +20,139 @@ impl Reducible for Measurement {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use reducible::Reducible;
+    use measurement::Measurement;
+
+    macro_rules! validate_reduce_value {
+        ($test_name:ident, $measurement_value:expr, $unit_str:expr, $expected_value:expr) => {
+            #[test]
+            fn $test_name() {
+                let measurement = Measurement::new($measurement_value, $unit_str).unwrap();
+                assert_relative_eq!(measurement.reduce_value(1.0), $expected_value);
+            }
+        };
+    }
+
+    macro_rules! validate_calculate_magnitude {
+        ($test_name:ident, $measurement_value:expr, $unit_str:expr, $expected_value:expr) => {
+            #[test]
+            fn $test_name() {
+                let measurement = Measurement::new($measurement_value, $unit_str).unwrap();
+                assert_relative_eq!(measurement.calculate_magnitude(1.0), $expected_value);
+            }
+        };
+    }
+
+    // reduce_value tests
+    validate_reduce_value!(validate_reduce_value_m, 1.0, "m", 1.0);
+    validate_reduce_value!(validate_reduce_value_km, 1.0, "km", 1000.0);
+    validate_reduce_value!(
+        validate_reduce_value_meter_minus1,
+        1.0,
+        "m-1",
+        1.0
+    );
+    validate_reduce_value!(
+        validate_reduce_value_meter_factor,
+        1.0,
+        "10m",
+        10.0
+    );
+    validate_reduce_value!(
+        validate_reduce_value_kilometer_factor,
+        1.0,
+        "10km",
+        10_000.0
+    );
+    validate_reduce_value!(
+        validate_reduce_value_kilometer_factor_exponent,
+        1.0,
+        "10km-1",
+        0.0001
+    );
+    validate_reduce_value!(validate_reduce_value_liter, 1.0, "L", 0.001);
+    validate_reduce_value!(
+        validate_reduce_value_pi,
+        1.0,
+        "[pi]",
+        ::std::f64::consts::PI
+    );
+    validate_reduce_value!(
+        validate_reduce_value_pi_factor,
+        1.0,
+        "10[pi]",
+        ::std::f64::consts::PI * 10.0
+    );
+    validate_reduce_value!(validate_reduce_value_hectare, 1.0, "har", 10_000.0);
+    validate_reduce_value!(validate_reduce_value_week, 1.0, "wk", 604_800.0);
+    validate_reduce_value!(validate_reduce_value_kilogram, 1.0, "kg", 1000.0);
+    validate_reduce_value!(
+        validate_reduce_value_fahrenheit,
+        1.0,
+        "[degF]",
+        255.927_777_777_777_8
+    );
+
+    // magnitude tests
+    validate_calculate_magnitude!(validate_calculate_magnitude_meter, 1.0, "m", 1.0);
+    validate_calculate_magnitude!(
+        validate_calculate_magnitude_kilometer,
+        1.0,
+        "km",
+        1000.0
+    );
+    validate_calculate_magnitude!(
+        validate_calculate_magnitude_meter_minus1,
+        1.0,
+        "m-1",
+        1.0
+    );
+    validate_calculate_magnitude!(
+        validate_calculate_magnitude_meter_factor,
+        1.0,
+        "10m",
+        10.0
+    );
+    validate_calculate_magnitude!(
+        validate_calculate_magnitude_kilometer_factor,
+        1.0,
+        "10km",
+        10_000.0
+    );
+    validate_calculate_magnitude!(
+        validate_calculate_magnitude_kilometer_factor_exponent,
+        1.0,
+        "10km-1",
+        0.000_1
+    );
+    validate_calculate_magnitude!(validate_calculate_magnitude_liter, 1.0, "L", 1.0);
+    validate_calculate_magnitude!(validate_calculate_magnitude_pi, 1.0, "[pi]", 1.0);
+    validate_calculate_magnitude!(
+        validate_calculate_magnitude_pi_factor,
+        1.0,
+        "10[pi]",
+        10.0
+    );
+    validate_calculate_magnitude!(
+        validate_calculate_magnitude_hectare,
+        1.0,
+        "har",
+        100.0
+    );
+    validate_calculate_magnitude!(validate_calculate_magnitude_week, 1.0, "wk", 1.0);
+    validate_calculate_magnitude!(
+        validate_calculate_magnitude_kilogram,
+        1.0,
+        "kg",
+        1000.0
+    );
+    validate_calculate_magnitude!(
+        validate_calculate_magnitude_fahrenheit,
+        1.0,
+        "[degF]",
+        1.000_000_000_000_056_8
+    );
+}
