598c30bc2dbe3972207c235139454856f011983f
Steve Loveless <steve@agrian.com>
2019-01-11 12:23:01 -0800

    DEV-4385 *Inverse traits add Output type
    ...allowing the Measurement implementation to handle DivideByZero errors.

--- api/src/measurement/invert.rs
+++ api/src/measurement/invert.rs
@@ -1,5 +1,5 @@
 use super::Measurement;
-use crate::invert::{Invert, ToInverse};
+use crate::{Error, invert::{Invert, ToInverse}};
 
 impl Invert for &mut Measurement {
     #[inline]
@@ -10,14 +10,20 @@ impl Invert for &mut Measurement {
 }
 
 impl ToInverse for Measurement {
+    type Output = Result<Measurement, Error>;
+
     #[inline]
-    fn to_inverse(&self) -> Measurement {
+    fn to_inverse(&self) -> Self::Output {
         let new_value = 1.0 / self.value;
 
-        Self {
+        if new_value.is_infinite() {
+            return Err(Error::DivideByZero);
+        }
+
+        Ok(Self {
             value: new_value,
             unit: self.unit.to_inverse(),
-        }
+        })
     }
 }
 
@@ -28,37 +34,37 @@ mod tests {
 
         #[test]
         fn validate_numerator_no_exponent() {
-            let mut unit = Measurement::new(10.0, "m").unwrap();
-            unit.invert();
-            assert_eq!(unit, Measurement::new(0.1, "m-1").unwrap());
+            let mut measurement = Measurement::new(10.0, "m").unwrap();
+            measurement.invert();
+            assert_eq!(measurement, Measurement::new(0.1, "m-1").unwrap());
         }
 
         #[test]
         fn validate_numerator_with_exponent_1() {
-            let mut unit = Measurement::new(10.0, "m1").unwrap();
-            unit.invert();
-            assert_eq!(unit, Measurement::new(0.1, "m-1").unwrap());
+            let mut measurement = Measurement::new(10.0, "m1").unwrap();
+            measurement.invert();
+            assert_eq!(measurement, Measurement::new(0.1, "m-1").unwrap());
         }
 
         #[test]
         fn validate_denominator_with_exponent_minus_1() {
-            let mut unit = Measurement::new(10.0, "m-1").unwrap();
-            unit.invert();
-            assert_eq!(unit, Measurement::new(0.1, "m").unwrap());
+            let mut measurement = Measurement::new(10.0, "m-1").unwrap();
+            measurement.invert();
+            assert_eq!(measurement, Measurement::new(0.1, "m").unwrap());
         }
 
         #[test]
         fn validate_numerator_and_denominator() {
-            let mut unit = Measurement::new(10.0, "m2/s2").unwrap();
-            unit.invert();
-            assert_eq!(unit, Measurement::new(0.1, "s2/m2").unwrap());
+            let mut measurement = Measurement::new(10.0, "m2/s2").unwrap();
+            measurement.invert();
+            assert_eq!(measurement, Measurement::new(0.1, "s2/m2").unwrap());
         }
 
         #[test]
         fn validate_numerators_and_denominators_mixed() {
-            let mut unit = Measurement::new(10.0, "m2/s2.g4/km4/har5").unwrap();
-            unit.invert();
-            assert_eq!(unit, Measurement::new(0.1, "s2.g4.har5/m2.km4").unwrap());
+            let mut measurement = Measurement::new(10.0, "m2/s2.g4/km4/har5").unwrap();
+            measurement.invert();
+            assert_eq!(measurement, Measurement::new(0.1, "s2.g4.har5/m2.km4").unwrap());
         }
     }
 
@@ -67,44 +73,44 @@ mod tests {
 
         #[test]
         fn validate_numerator_no_exponent() {
-            let unit = Measurement::new(10.0, "m").unwrap();
-            let new_unit = unit.to_inverse();
-            assert_eq!(new_unit, Measurement::new(0.1, "m-1").unwrap());
+            let measurement = Measurement::new(10.0, "m").unwrap();
+            let new_measurement = measurement.to_inverse().unwrap();
+            assert_eq!(new_measurement, Measurement::new(0.1, "m-1").unwrap());
         }
 
         #[test]
         fn validate_numerator_with_exponent_1() {
-            let unit = Measurement::new(10.0, "m1").unwrap();
-            let new_unit = unit.to_inverse();
-            assert_eq!(new_unit, Measurement::new(0.1, "m-1").unwrap());
+            let measurement = Measurement::new(10.0, "m1").unwrap();
+            let new_measurement = measurement.to_inverse().unwrap();
+            assert_eq!(new_measurement, Measurement::new(0.1, "m-1").unwrap());
         }
 
         #[test]
         fn validate_denominator_with_exponent_minus_1() {
-            let unit = Measurement::new(10.0, "m-1").unwrap();
-            let new_unit = unit.to_inverse();
-            assert_eq!(new_unit, Measurement::new(0.1, "m").unwrap());
+            let measurement = Measurement::new(10.0, "m-1").unwrap();
+            let new_measurement = measurement.to_inverse().unwrap();
+            assert_eq!(new_measurement, Measurement::new(0.1, "m").unwrap());
         }
 
         #[test]
         fn validate_numerator_and_denominator() {
-            let unit = Measurement::new(10.0, "m2/s2").unwrap();
-            let new_unit = unit.to_inverse();
-            assert_eq!(new_unit, Measurement::new(0.1, "s2/m2").unwrap());
+            let measurement = Measurement::new(10.0, "m2/s2").unwrap();
+            let new_measurement = measurement.to_inverse().unwrap();
+            assert_eq!(new_measurement, Measurement::new(0.1, "s2/m2").unwrap());
         }
 
         #[test]
         fn validate_numerators_and_denominators_mixed() {
-            let unit = Measurement::new(10.0, "m2/s2.g4/km4/har5").unwrap();
-            let new_unit = unit.to_inverse();
-            assert_eq!(new_unit, Measurement::new(0.1, "s2.g4.har5/m2.km4").unwrap());
+            let measurement = Measurement::new(10.0, "m2/s2.g4/km4/har5").unwrap();
+            let new_measurement = measurement.to_inverse().unwrap();
+            assert_eq!(new_measurement, Measurement::new(0.1, "s2.g4.har5/m2.km4").unwrap());
         }
 
         #[test]
         fn validate_zero_value() {
-            let unit = Measurement::new(0.0, "m2/s2.g4/km4/har5").unwrap();
-            let new_unit = unit.to_inverse();
-            assert_eq!(new_unit, Measurement::new(std::f64::INFINITY, "s2.g4.har5/m2.km4").unwrap());
+            let measurement = Measurement::new(0.0, "m2/s2.g4/km4/har5").unwrap();
+            let new_measurement = measurement.to_inverse();
+            assert_eq!(new_measurement, Err(crate::Error::DivideByZero));
         }
     }
 
@@ -113,37 +119,37 @@ mod tests {
 
         #[test]
         fn validate_numerator_no_exponent() {
-            let unit = Measurement::new(10.0, "m").unwrap();
-            let new_unit = unit.into_inverse();
-            assert_eq!(new_unit, Measurement::new(0.1, "m-1").unwrap());
+            let measurement = Measurement::new(10.0, "m").unwrap();
+            let new_measurement = measurement.into_inverse().unwrap();
+            assert_eq!(new_measurement, Measurement::new(0.1, "m-1").unwrap());
         }
 
         #[test]
         fn validate_numerator_with_exponent_1() {
-            let unit = Measurement::new(10.0, "m1").unwrap();
-            let new_unit = unit.into_inverse();
-            assert_eq!(new_unit, Measurement::new(0.1, "m-1").unwrap());
+            let measurement = Measurement::new(10.0, "m1").unwrap();
+            let new_measurement = measurement.into_inverse().unwrap();
+            assert_eq!(new_measurement, Measurement::new(0.1, "m-1").unwrap());
         }
 
         #[test]
         fn validate_denominator_with_exponent_minus_1() {
-            let unit = Measurement::new(10.0, "m-1").unwrap();
-            let new_unit = unit.into_inverse();
-            assert_eq!(new_unit, Measurement::new(0.1, "m").unwrap());
+            let measurement = Measurement::new(10.0, "m-1").unwrap();
+            let new_measurement = measurement.into_inverse().unwrap();
+            assert_eq!(new_measurement, Measurement::new(0.1, "m").unwrap());
         }
 
         #[test]
         fn validate_numerator_and_denominator() {
-            let unit = Measurement::new(10.0, "m2/s2").unwrap();
-            let new_unit = unit.into_inverse();
-            assert_eq!(new_unit, Measurement::new(0.1, "s2/m2").unwrap());
+            let measurement = Measurement::new(10.0, "m2/s2").unwrap();
+            let new_measurement = measurement.into_inverse().unwrap();
+            assert_eq!(new_measurement, Measurement::new(0.1, "s2/m2").unwrap());
         }
 
         #[test]
         fn validate_numerators_and_denominators_mixed() {
-            let unit = Measurement::new(10.0, "m2/s2.g4/km4/har5").unwrap();
-            let new_unit = unit.into_inverse();
-            assert_eq!(new_unit, Measurement::new(0.1, "s2.g4.har5/m2.km4").unwrap());
+            let measurement = Measurement::new(10.0, "m2/s2.g4/km4/har5").unwrap();
+            let new_measurement = measurement.into_inverse().unwrap();
+            assert_eq!(new_measurement, Measurement::new(0.1, "s2.g4.har5/m2.km4").unwrap());
         }
     }
 }
