8daae3be32ee6a3a533911a88134a8c874b8c36a
Steve Loveless <steve.loveless@agrian.com>
2023-07-07 15:22:51 -0700

    Derive v2 UcumUnit traits for existing impls of UcumUnit

--- api/src/v2/ucum_unit.rs
+++ api/src/v2/ucum_unit.rs
@@ -1,4 +1,6 @@
 #![allow(clippy::module_name_repetitions)]
+//! These traits are a progression from the existing `crate::UcumUnit` trait, allowing the same
+//! functionality, but also allowing downstream crates to implement for wrapper types.
 
 pub trait UcumUnitFlags {
     fn is_special(&self) -> bool;
@@ -6,7 +8,37 @@ pub trait UcumUnitFlags {
     fn is_arbitrary(&self) -> bool;
 }
 
+impl<T> UcumUnitFlags for T
+where
+    T: crate::UcumUnit,
+{
+    fn is_special(&self) -> bool {
+        crate::UcumUnit::is_special(self)
+    }
+
+    fn is_metric(&self) -> bool {
+        crate::UcumUnit::is_metric(self)
+    }
+
+    fn is_arbitrary(&self) -> bool {
+        crate::UcumUnit::is_arbitrary(self)
+    }
+}
+
 pub trait UcumUnitComputedValues<T> {
     fn scalar(&self) -> T;
     fn magnitude(&self) -> T;
+}
+
+impl<T> UcumUnitComputedValues<f64> for T
+where
+    T: crate::UcumUnit,
+{
+    fn scalar(&self) -> f64 {
+        crate::UcumUnit::scalar(self)
+    }
+
+    fn magnitude(&self) -> f64 {
+        crate::UcumUnit::magnitude(self)
+    }
 }
