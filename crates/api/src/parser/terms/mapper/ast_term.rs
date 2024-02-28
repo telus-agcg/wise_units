bc51dbe7beb8bb160124244e2f33346980053f69
Steve Loveless <steve.loveless@telusagcg.com>
2024-02-28 12:23:22 -0800

    PLCC-299 Refactor ast_term parsing

--- crates/api/src/parser/terms/mapper/ast_term.rs
+++ crates/api/src/parser/terms/mapper/ast_term.rs
@@ -1,4 +1,4 @@
-use pest::iterators::Pair;
+use pest::iterators::{Pair, Pairs};
 
 use crate::parser::terms::term_parser::Rule;
 
@@ -11,54 +11,68 @@ pub(super) struct AstTerm {
 
 impl AstTerm {
     pub(super) fn visit(pair: Pair<'_, Rule>) -> Result<Self, Error> {
+        fn parse_component(pairs: &mut Pairs<'_, Rule>) -> Result<Component, Error> {
+            pairs.next().map_or_else(
+                || unreachable!(),
+                |first| match first.as_rule() {
+                    Rule::component => Component::visit(first),
+                    _ => unreachable!(),
+                },
+            )
+        }
+
+        fn parse_dot_or_slash(pairs: &mut Pairs<'_, Rule>) -> Option<SecondToken> {
+            match pairs.next()?.as_rule() {
+                Rule::dot => Some(SecondToken::Dot),
+                Rule::slash => Some(SecondToken::Slash),
+                _ => unreachable!(),
+            }
+        }
+
+        fn parse_tail(
+            component: Component,
+            op: SecondToken,
+            pairs: &mut Pairs<'_, Rule>,
+        ) -> Result<AstTerm, Error> {
+            match pairs.next() {
+                Some(third) => match third.as_rule() {
+                    Rule::term => {
+                        let mut new_terms: Vec<Term> = AstTerm::visit(third)?.finish();
+
+                        match op {
+                            SecondToken::Dot => (),
+                            SecondToken::Slash => {
+                                crate::parser::term::num_traits::inv::inv_terms(&mut new_terms);
+                            }
+                        }
+
+                        Ok(AstTerm {
+                            component,
+                            terms: new_terms,
+                        })
+                    }
+                    _ => unreachable!(),
+                },
+                None => unreachable!(),
+            }
+        }
+
         let mut pairs = pair.into_inner();
 
-        let component = match pairs.next() {
-            Some(first) => match first.as_rule() {
-                Rule::component => Component::visit(first)?,
-                _ => unreachable!(),
-            },
-            None => unreachable!(),
+        let component = parse_component(&mut pairs)?;
+
+        let Some(op) = parse_dot_or_slash(&mut pairs) else {
+            return Ok(Self {
+                component,
+                terms: Vec::with_capacity(0),
+            });
         };
 
-        let op = match pairs.next() {
-            Some(second) => match second.as_rule() {
-                Rule::dot => SecondToken::Dot,
-                Rule::slash => SecondToken::Slash,
-                _ => unreachable!(),
-            },
-            None => {
-                return Ok(Self {
-                    component,
-                    terms: Vec::with_capacity(0),
-                });
-            }
-        };
-
-        match pairs.next() {
-            Some(third) => match third.as_rule() {
-                Rule::term => {
-                    let mut new_terms: Vec<Term> = Self::visit(third)?.finish();
-
-                    match op {
-                        SecondToken::Dot => (),
-                        SecondToken::Slash => {
-                            crate::parser::term::num_traits::inv::inv_terms(&mut new_terms);
-                        }
-                    }
-
-                    Ok(Self {
-                        component,
-                        terms: new_terms,
-                    })
-                }
-                _ => unreachable!(),
-            },
-            None => unreachable!(),
-        }
+        parse_tail(component, op, &mut pairs)
     }
 }
 
+#[derive(Clone, Copy)]
 enum SecondToken {
     Dot,
     Slash,
