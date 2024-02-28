1706e79b82cd6da970abfb941f2f3b4544849da4
Steve Loveless <steve.loveless@telusagcg.com>
2024-02-28 12:07:01 -0800

    PLCC-299 Refactor main_term parsing
    Slight 2-3% bump here

--- crates/api/src/parser/terms/mapper/main_term.rs
+++ crates/api/src/parser/terms/mapper/main_term.rs
@@ -1,5 +1,5 @@
 use num_traits::Inv;
-use pest::iterators::Pair;
+use pest::iterators::{Pair, Pairs};
 
 use crate::parser::{terms::term_parser::Rule, Error, Term};
 
@@ -10,40 +10,57 @@ pub(super) struct MainTerm {
 }
 
 impl Visit<Rule> for MainTerm {
+    // NOTE: Look at `term.pest` to see that parsing a `MainTerm` can only result in 2 options:
+    // 1. a slash-term (ex. "/100m{stuff}.[ft_i]")
+    // 2. a term (ex. "100m{stuff}.[ft_i]/cm")
+    //
+    // ...which means our first `Pairs` match can either be 1. a slash or 2. a term; if it's 1,
+    // then we're in case 1 above; if it's 2 then we're in case 2 above.
     fn visit(pair: Pair<'_, Rule>) -> Result<Self, Error> {
-        let mut pairs = pair.into_inner();
-
-        // match first token
-        match pairs.next() {
-            Some(first) => match first.as_rule() {
-                Rule::term => {
-                    return Ok(Self {
-                        terms: AstTerm::visit(first)?.finish(),
-                    });
-                }
-                // Don't do anything, but because we proceed through this method, we can later
-                // assume that we parsed a slash.
-                Rule::slash => (),
-                _ => unreachable!(),
-            },
-            None => unreachable!(),
+        enum OneOrTwoTerms {
+            Term(MainTerm),
+            Slash,
         }
 
-        match pairs.next() {
-            Some(second) => match second.as_rule() {
-                Rule::term => {
-                    let terms: Vec<Term> = AstTerm::visit(second)?.finish();
+        fn parse_first(pairs: &mut Pairs<'_, Rule>) -> Result<OneOrTwoTerms, Error> {
+            match pairs.next() {
+                Some(first) => match first.as_rule() {
+                    Rule::term => Ok(OneOrTwoTerms::Term(MainTerm {
+                        terms: AstTerm::visit(first)?.finish(),
+                    })),
+                    // Don't do anything, but because we proceed through this method, we can later
+                    // assume that we parsed a slash.
+                    Rule::slash => Ok(OneOrTwoTerms::Slash),
+                    _ => unreachable!(),
+                },
+                None => unreachable!(),
+            }
+        }
 
-                    // If we're here it's because there was a leading slash, so invert.
-                    let u = crate::Unit::new(terms).inv();
+        fn parse_second(pairs: &mut Pairs<'_, Rule>) -> Result<MainTerm, Error> {
+            match pairs.next() {
+                Some(second) => match second.as_rule() {
+                    Rule::term => {
+                        let terms: Vec<Term> = AstTerm::visit(second)?.finish();
 
-                    Ok(Self {
-                        terms: u.into_terms().to_vec(),
-                    })
-                }
-                _ => unreachable!(),
-            },
-            None => unreachable!(),
+                        // If we're here it's because there was a leading slash, so invert.
+                        let u = crate::Unit::new(terms).inv();
+
+                        Ok(MainTerm {
+                            terms: u.into_terms().to_vec(),
+                        })
+                    }
+                    _ => unreachable!(),
+                },
+                None => unreachable!(),
+            }
+        }
+
+        let mut pairs = pair.into_inner();
+
+        match parse_first(&mut pairs)? {
+            OneOrTwoTerms::Term(main_term) => Ok(main_term),
+            OneOrTwoTerms::Slash => parse_second(&mut pairs),
         }
     }
 }
