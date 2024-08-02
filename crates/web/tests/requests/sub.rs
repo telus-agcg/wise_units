6f0395552c920fada43cd27a6001b39623edd1e7
Steve Loveless <steve.loveless@telusagcg.com>
2024-08-01 23:11:23 -0700

    Add locoer command

--- crates/web/tests/requests/sub.rs
+++ crates/web/tests/requests/sub.rs
@@ -1,29 +1,40 @@
-use wise_units_web::app::App;
 use loco_rs::testing;
 use serial_test::serial;
+use wise_units_web::app::App;
 
 #[tokio::test]
 #[serial]
-async fn can_get_echo() {
+async fn can_post_sub_root() {
     testing::request::<App, _, _>(|request, _ctx| async move {
         let payload = serde_json::json!({
-            "foo": "bar",
+            "lhs": {
+                "value": 1,
+                "unit": "km2"
+            },
+            "rhs": {
+                "value": 1,
+                "unit": "km2"
+            }
         });
 
-        let res = request.post("/sub/echo").json(&payload).await;
+        let expected = serde_json::json!({
+            "value": 0.0,
+            "unit": "km2"
+        });
+        let res = request.post("/api/sub").json(&payload).await;
         assert_eq!(res.status_code(), 200);
-        assert_eq!(res.text(), serde_json::to_string(&payload).unwrap());
+        assert_eq!(res.text(), serde_json::to_string(&expected).unwrap());
     })
     .await;
 }
 
 #[tokio::test]
 #[serial]
-async fn can_request_root() {
+async fn can_not_get_request_root() {
     testing::request::<App, _, _>(|request, _ctx| async move {
-        let res = request.get("/sub").await;
-        assert_eq!(res.status_code(), 200);
-        assert_eq!(res.text(), "hello");
+        let res = request.get("/api/sub").await;
+        assert_eq!(res.status_code(), 405);
     })
     .await;
 }
+
